fc650b735a3166d5656ca509d98c97322a5d365c
josh <jjhouston24@gmail.com>
2019-06-10 21:48:04 -0500

    react-nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './reset.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      menuStatus: 'drop-down-menu',
+      sideStatus: 'drop=down-side'
+    }
+  }
+
+  handleClick = () => {
+    if(this.state.menuStatus === 'drop-down-menu-open' || this.state.sideStatus === 'drop-down-side-open') {
+      this.setState({menuStatus: 'drop-down-menu-closed'})
+      this.setState({sideStatus: 'drop-down-side-closed'})
+    }else {
+      this.setState({menuStatus: 'drop-down-menu-open'})
+      this.setState({sideStatus: 'drop-down-side-open'})
+    }
+    
+  }
+  render() {
+    return (
+      <header>
+        <nav>
+          <ul>
+            <div className='logo'>
+              <li>Start Bootstrap</li>
+            </div>
+              <li className='elements'>SERVICES</li>
+              <li className='elements'>PORTFOLIO</li>
+              <li className='elements'>ABOUT</li>
+              <li className='elements'>TEAM</li>
+              <li className='elements'>CONTACT</li>
+
+              <div className="hamburger" >
+                <li className='menuham'>
+                  MENU
+                  <img 
+                   onClick={this.handleClick}
+                  src="https://img.icons8.com/material/24/000000/menu.png"/>
+                </li>
+            </div>
+            
+            <div className={this.state.menuStatus}>
+              <div className={this.state.sideStatus}>
+                <li>SERVICES</li>
+                <li>PORTFOLIO</li>
+                <li>ABOUT</li>
+                <li>TEAM</li>
+                <li>CONTACT</li>
+              </div>
+            </div>
+          </ul>
+        </nav>
       </header>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
